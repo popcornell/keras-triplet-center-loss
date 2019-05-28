8d8fb64aa17bd63b5763fe52de96307c7ffeb4b2
popcornell <cornellsamuele@gmail.com>
2019-05-28 15:10:06 +0200

    softmax has problems

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # HYPERPARAMS
 batch_size = 64
-n_epochs = 30
+n_epochs = 10
 
 
 # loading MNIST
@@ -38,7 +38,7 @@ from tensorflow.keras.models import Model
 
 model_input = Input(shape=(28,28, 1))
 
-from cnn import cnn, generate_triplet, triplet_loss
+from cnn import cnn, generate_triplet, center_triplet_loss
 
 softmax, pre_logits = cnn(model_input)
 
@@ -47,38 +47,55 @@ x_test_flat = x_test.reshape(-1,784)
 
 X_train, Y_train = generate_triplet(x_train_flat,y_train, ap_pairs=150, an_pairs=150)
 
-shared_model = tf.keras.models.Model(inputs=model_input, outputs=[softmax, pre_logits])
 
-from tensorflow.keras.layers import concatenate
+from tensorflow.keras.layers import concatenate, Lambda, Embedding
+import tensorflow.keras.backend as K
+
+
+target_input = Input((1, ), name='target_input')
+
+center = Embedding(10, 32 )(target_input)
+l2_loss = Lambda(lambda x: K.sum(K.square(x[0] - x[1][:, 0]), 1, keepdims=True), name='l2_loss_anchor')([pre_logits, center])
+
+
+shared_model = tf.keras.models.Model(inputs=[model_input, target_input], outputs=[softmax,  pre_logits])
+
+
 
 anchor_input = Input((28,28,1, ), name='anchor_input')
 positive_input = Input((28,28,1, ), name='positive_input')
 negative_input = Input((28,28,1, ), name='negative_input')
 
-soft_anchor, encoded_anchor = shared_model(anchor_input)
-soft_pos, encoded_positive = shared_model(positive_input)
-soft_neg, encoded_negative = shared_model(negative_input)
 
-merged_vector = concatenate([encoded_anchor, encoded_positive, encoded_negative], axis=-1, name='merged_layer')
+target_anchor_input = Input((1, ), name="anchor_target_input")
+target_positive_input = Input((1, ), name='target_positive_input')
+target_negative_input = Input((1, ), name='target_negative_input')
+
+
+soft_anchor,  pre_logits_anchor = shared_model([anchor_input, target_anchor_input])
+soft_pos, pre_logits_pos = shared_model([positive_input, target_positive_input])
+soft_neg, pre_logits_neg = shared_model([negative_input, target_negative_input])
+
+# center triplet loss
+
+#center_anchor = Embedding(10, 32 )(target_anchor_input)
+#center_pos = Embedding(10, 32 )(target_positive_input)
+#center_neg = Embedding(10, 32 )(target_negative_input)
+
+#l2_loss_anchor = Lambda(lambda x: K.sum(K.square(x[0] - x[1][:, 0]), 1, keepdims=True), name='l2_loss_anchor')([encoded_anchor, center_anchor])
+#l2_loss_pos = Lambda(lambda x: K.sum(K.square(x[0] - x[1][:, 0]), 1, keepdims=True), name='l2_loss_pos')([encoded_positive, center_pos])
+#l2_loss_neg = Lambda(lambda x: K.sum(K.square(x[0] - x[1][:, 0]), 1, keepdims=True), name='l2_loss_neg')([encoded_negative, center_neg])
+
+#merged_l2 = concatenate([l2_anchor, l2_positive, l2_negative], axis=-1, name="merged_l2")
+#merged_centers = concatenate([ center_anchor, center_pos, center_neg], axis=-1, name='merged_centers')
+#merged_encodings = concatenate([encoded_anchor, encoded_positive, encoded_negative], axis=-1, name='merged_encodings')
+
 merged_soft = concatenate([soft_anchor, soft_pos, soft_neg], axis=-1, name='merged_soft')
 
-def xentropy(y_true, y_pred):
 
-    total_lenght = y_pred.shape.as_list()[-1]
-
-    anchor_pred = y_pred[:, 0:int(total_lenght * 1 / 3)]
-    positive_pred = y_pred[:, int(total_lenght * 1 / 3):int(total_lenght * 2 / 3)]
-    negative_pred = y_pred[:, int(total_lenght * 2 / 3):int(total_lenght * 3 / 3)]
-
-    anchor_true = y_pred[:, 0:int(total_lenght * 1 / 3)]
-    positive_true = y_pred[:, int(total_lenght * 1 / 3):int(total_lenght * 2 / 3)]
-    negative_true = y_pred[:, int(total_lenght * 2 / 3):int(total_lenght * 3 / 3)]
-
-
-model = Model(inputs=[anchor_input, positive_input, negative_input], outputs=[merged_vector, merged_soft])
-model.compile(loss=[triplet_loss, "categorical_crossentropy"], optimizer=tf.keras.optimizers.Adam(lr=0.05), metrics=["accuracy"],
-              loss_weights=[0.7, 0.3])
-
+model = Model(inputs=[anchor_input, positive_input, negative_input, target_anchor_input, target_positive_input, target_negative_input], outputs=[merged_soft])
+model.compile(loss=["categorical_crossentropy"], optimizer=tf.keras.optimizers.Adam(lr=0.001), metrics=["accuracy"],
+              )
 
 import numpy as np
 
@@ -94,8 +111,19 @@ Y_Negative = le.fit_transform(Y_train[:, 2])
 target = np.concatenate((Y_Anchor, Y_Positive, Y_Negative), -1)
 
 
-model.fit([Anchor, Positive, Negative],y=[target, target], batch_size=batch_size, epochs=n_epochs)
+model.fit([Anchor, Positive, Negative, Y_train[:, 0], Y_train[:, 1], Y_train[:, 2] ],y=[target], batch_size=batch_size, epochs=n_epochs)
 
+model.save("tripletloss_xentropy_model.h5")
+
+model = Model(inputs=[anchor_input, target_anchor_input], outputs= [soft_anchor, l2_anchor, pre_logits_anchor])
+model.load_weights("tripletloss_xentropy_model.h5")
+
+_, _, X_train_embed = model.predict([x_train[:512], y_train[:512]])
+_, _, X_test_embed = model.predict([x_test[:512], y_train[:512]])
+
+from TSNE_plot import tsne_plot
+
+tsne_plot(X_train_embed, y_train, X_test_embed, y_test, "xentropy triplet")
 
 
 
